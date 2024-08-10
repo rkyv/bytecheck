d94da6c705ac48fcf461ab5e5bab861a49ca229f
Stepan Tubanov <stepan773@gmail.com>
2024-08-10 04:01:38 +0400

    Fix doctests and add a note about type layout (#36)

--- bytecheck/src/lib.rs
+++ bytecheck/src/lib.rs
@@ -18,6 +18,13 @@
 //! verifying that some bytes represent a valid type. Implementing it can be
 //! done manually or automatically with the [derive macro](macro@CheckBytes).
 //!
+//! ## Note
+//!
+//! By default in-memory layout of a type may change between compiler versions or
+//! different compilations (see <https://doc.rust-lang.org/reference/type-layout.html>).
+//! In order to guarantee stable type layout between compilations `#[repr(C)]` attribute
+//! can be used.
+//!
 //! ## Examples
 //!
 //! ```
@@ -957,3 +964,8 @@ impl_nonzero!(NonZeroU16, u16);
 impl_nonzero!(NonZeroU32, u32);
 impl_nonzero!(NonZeroU64, u64);
 impl_nonzero!(NonZeroU128, u128);
+
+// Make sure code in readme is tested. This isn't included in the documentation.
+#[doc = include_str!("../../README.md")]
+#[cfg(doctest)]
+pub struct ReadmeDoctests;
